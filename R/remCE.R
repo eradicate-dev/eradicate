ab728220263625c3c4a6e0baa9f5b3d260c28754
Dave Ramsey <ramseydsl@gmail.com>
2022-03-01 09:07:38 +1100

    added otis estimator to remCE
    added zzz.r to unload .dll

--- R/remCE.R
+++ R/remCE.R
@@ -28,20 +28,17 @@
 #'
 #' @export
 #'
-remCE<- function (catch = NULL, effort = NULL,
-                  method = c("l", "ml", "hosc", "hesc", "hemqle"),
-                  nboot = 500)
+remCE<- function (data, method = c("l", "ml", "hosc", "hesc", "hemqle"), nboot = 500, kwh=NULL)
 {
   if (!any(method %in% c("l", "ml","hosc", "hesc", "hemqle")))
     stop("not a valid method.")
-  if (is.null(catch))
+  if (is.null(x$catch))
     stop("catch vector does not exist.")
   if (is.null(effort))
     stop("effort vector does not exist.")
   if (length(catch) != length(effort))
     stop("unequal catch/effort vector lengths.")
-  x <- as.data.frame(cbind(catch, effort))
-  names(x) <- c("catch", "effort")
+  x <- data$counts
   x$samp <- seq(1, length(x$catch), 1)
   x$cpue <- x$catch/x$effort
   x$K <- cumsum(x$catch) - x$catch
@@ -58,7 +55,6 @@ remCE<- function (catch = NULL, effort = NULL,
     if (length(x$catch) < 3)
       stop("Leslie method requires at least 3 observations!")
     ld <- lm(cpue ~ K, data = x)
-    l.out <- NULL
     names(ld$coefficients)[2] <- "q"
     Nld <- -coef(summary(ld))[1]/coef(summary(ld))[2]
     qld <- -coef(summary(ld))[2]
@@ -409,13 +405,162 @@ remCE<- function (catch = NULL, effort = NULL,
     UCn <- sum(dd$catch) + (Nmqle - sum(dd$catch)) * C
     ans <- NULL
     ans$results <- matrix(NA, 2L, 7L)
-    ans$results <- rbind(cbind(round(Nmqle, 0), round(seN,
-                                                      1), round(LCn, 1), round(UCn, 1), round(CVNmqle,
-                                                                                              3)), cbind(q, seqq, NA, NA, NA))
+    ans$results <- rbind(cbind(round(Nmqle, 0),
+                               round(seN,1), round(LCn, 1), round(UCn, 1),
+                               round(CVNmqle,3)), cbind(q, seqq, NA, NA, NA))
     dimnames(ans$results) <- list(cbind("N", "lambda"),
                                   c("Estimate", "SE", "95% LCI",
                                     "95% UCI", " lambda CV"))
     out <- ans
+  }
+  if(any(method=="wh")){
+    if(length(x$catch) == 2){
+      if(x$catch[2] >= x$catch[1]) warning ("For the two-pass estimator,
+                                            catch of first pass must be greater
+                                            than catch of second pass.")
+    }
+    nsam<- length(x$catch)
+    endk<- ifelse(is.null(kwh), as.numeric(nsam-2), kwh)
+    endk<- ifelse(endk > as.numeric(nsam-2),as.numeric(nsam-2),endk)
+    if(nsam==2){
+      warning ("Two pass estimator and variance of Otis et al. 1978 used.")
+      N2<- x$catch[1]/(1-(x$catch[2]/x$catch[1]))
+      q1<- 1-(x$catch[2]/x$catch[1])
+      SEO<- sqrt(((N2*(1-q1)^nsam)*(1-(1-q1)^nsam))/((1-(1-q1)^nsam)^2-(nsam^2*q1^2*(1-q1)^(nsam-1))))
+      ans<- NULL
+      ans$two_pass<- matrix(NA,2L,2L)
+      ans$two_pass<- rbind(cbind(round(N2,1),round(SEO,2)),
+                           cbind(q1,NA))
+      dimnames(ans$two_pass)<- list(cbind("N","q1"),c("Estimate","SE"))
+    }
+    if(nsam > 2){
+      pcatch1<- c(rep(0,nsam))
+      T<- c(rep(0,nsam))
+      qq<- seq(1,nsam,1)
+      T[c(seq(1,nsam,1))]<- cumsum(x$catch[c(seq(1,nsam,1))])
+      TC<-sum(x$catch)
+      if(is.null(Nstart)) N<-TC
+      if(!is.null(Nstart)) N<-Nstart
+      ans<-NULL
+      resids<-NULL
+      preds<-NULL
+      lls<-NULL
+
+      for(k in 1:endk){# limited to n-p
+        if(k == 1) ps<- T[nsam]/(nsam*N-sum(T[1:nsam-1]))
+        if(k >= 2) ps<- rep(T[nsam]/(nsam*N-sum(T[1:nsam-1])),k)
+        parms<- c(N,ps)
+        model<-function(y){
+          N<- y[1]
+          ps<- y[2:length(y)]
+          pp<-NULL
+          pp<- array(NA,dim=c(nsam,k))
+          for(nc in 1:ncol(pp)){
+            for(nr in nc:nsam){
+              if(k == nc) pp[nr,nc]<- ps[nc]*(1-ps[nc])^(nr-nc)
+              if(nc < k){
+                if(nr == nc) pp[nr,nc]<- ps[nc]
+                if(nr > nc) pp[nr,nc]<- 1-ps[nc]
+              }
+            }
+          }
+          pps<- apply(pp,1,prod,na.rm=T)
+          pcatch1<- N*pps
+          pT<- sum(pcatch1)
+          L1<- sum(log((seq(1,T[nsam],1)+N-T[nsam])/seq(1,T[nsam],1)))
+          ratio<-NULL
+          for(f in 1:nsam){
+            if(x$catch[f] <= 0 & pcatch1[f] > 0) ratio[f]<- 0
+            if(x$catch[f] <= 0 & pcatch1[f] <= 0) ratio[f]<- 0
+            if(x$catch[f] > 0 & pcatch1[f] <= 0) ratio[f]<- 0
+            if(x$catch[f] > 0 & pcatch1[f] > 0) ratio[f]<- log(x$catch[f]/pcatch1[f])
+          }
+
+          H<- sum(x$catch*ratio)
+          dNTP<- ifelse(as.numeric(N-pT)<=0,0,log(N-pT))
+          G<- N*log(N)-T[nsam]*log(T[nsam])-(N-T[nsam])*dNTP-L1
+          G+H
+        }
+        lower<- c(sum(x$catch),rep(1e-15,k))
+        upper<- c(sum(x$catch)*20,rep(1,k))                 #upper limits of N and q
+        j<-0
+        # try different starting value if model doesn't fit at first
+        repeat{
+          results<-try(optim(parms, model, gr = NULL,lower=lower,upper=upper,method=c("L-BFGS-B"),
+                             control=list(maxit=100000),hessian=TRUE),TRUE)
+          if(is(results,"try-error")){
+            j<-j+1
+            parms<-c(parms[1]*(1+j/10),parms[2:length(parms)])
+            if(j==30) break
+          }
+          if(!is(results,"try-error")) break
+        }
+
+        if(!is(results,"try-error")){
+          cov1<- solve(results$hessian)
+          # in case SE is estimated inadequately
+          var<- ifelse(diag(cov1)<0,NA,diag(cov1))
+          SEs<- sqrt(var)
+          ps<- results$par[2:length(results$par)]
+          pp<- NULL
+          pp<- array(NA,dim=c(nsam,length(ps)))
+          for(nc in 1:ncol(pp)){
+            for(nr in nc:nsam){
+              if(k == nc) pp[nr,nc]<-ps[nc]*(1-ps[nc])^(nr-nc)
+              if(nc < k){
+                if(nr == nc) pp[nr,nc]<-ps[nc]
+                if(nr > nc) pp[nr,nc]<-1-ps[nc]
+              }
+            }
+          }
+          pps<- apply(pp,1,prod,na.rm=T)
+          pcatch1<- results$par[1]*pps
+          resid<- x$catch - pcatch1
+          chi<- ifelse(is.nan(((x$catch-pcatch1)^2)/pcatch1),0,((x$catch-pcatch1)^2)/pcatch1)
+          chi<-sum(chi)
+          df<- nsam - length(results$par)
+          prob<- 1-pchisq(chi,df)
+          aic<- 2*results$value+2*length(results$par)
+        }
+        if(is(results,"try-error")){
+          pcatch1<- rep(NA,nsam)
+          resid<- rep(NA,nsam)
+          chi<- NA
+          df<- NA
+          prob<- NA
+          SEs<- rep(NA,length(parms))
+        }
+        # Create CAPTURE TABLE
+        plabs<- paste("p",seq(1,length(ps),1),sep="")
+        if(!is(results,"try-error")) comb<- cbind(round(results$par,4),round(SEs,4))
+        if(is(results,"try-error")) comb<- cbind(rep(NA,length(parms)),round(SEs,4))
+        rownames(comb)<- c("N",plabs)
+        comb<- as.data.frame(comb)
+        names(comb)<- c("Estimate","SE")
+        comb$chi<- c(round(chi,3),rep(NA,length(ps)))
+        comb$df<- c(df,rep(NA,length(ps)))
+        comb$prob<- c(round(prob,4),rep(NA,length(ps)))
+        # Create Residuals, Observed, Predicted Table
+        resids<- cbind(resids,resid)
+        preds<- cbind(preds,pcatch1)
+        lls<- rbind(lls,aic)
+        ans[[k]]<- comb
+      }#Kloop
+      prc<- as.matrix(cbind(x$catch,preds))
+      klabs<- paste("k=",seq(1,endk,1),sep="")
+      colnames(prc)<- c("catch",klabs)
+      res<- as.matrix(resids)
+      colnames(res)<- c(klabs)
+      kl<- paste("k",seq(1,endk,1),sep="")
+      names(ans)<- c(kl)
+      aicout<- as.matrix(as.vector(lls),rownames=NA)
+      colnames(aicout)<- c("AIC Value")
+      rownames(aicout)<- c(seq(1,endk,1))
+      ans$aic<- aicout
+      ans$predicted<- prc
+      ans$residuals<- res
+    }
+    out<- ans
   }
   out
 }
